a13e6714516dea10ff8ec87241ee322b05a0c355
Ahmet Sait Çalışır <56252214+AhmetSaitCalisir@users.noreply.github.com>
2021-12-26 18:36:17 +0300

    Survey Share

--- client/src/components/Home/HomeMainContent.vue
+++ client/src/components/Home/HomeMainContent.vue
@@ -52,11 +52,56 @@
                     class="dropdown-menu"
                     aria-labelledby="dropdownMenuButton1"
                   >
-                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li>
+                      <a
+                        class="dropdown-item"
+                        href="#"
+                        @click.prevent="share(poll.id)"
+                        ><svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="15"
+                          height="15"
+                          fill="currentColor"
+                          class="bi bi-share"
+                          viewBox="0 0 20 20"
+                        >
+                          <path
+                            d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z"
+                          />
+                        </svg>
+                        Paylaş
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#"
+                        ><svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="15"
+                          height="15"
+                          fill="currentColor"
+                          class="bi bi-exclamation-triangle"
+                          viewBox="0 0 20 20"
+                        >
+                          <path
+                            d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z"
+                          />
+                          <path
+                            d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z"
+                          />
+                        </svg>
+                        Raporla
+                      </a>
+                    </li>
                   </ul>
                 </div>
               </div>
-              <div class="mute">1 minute ago</div>
+              <div class="mute">
+                {{
+                  new Date(poll.creationDate).toLocaleDateString() +
+                  " " +
+                  new Date(poll.creationDate).toLocaleTimeString()
+                }}
+              </div>
             </div>
 
             <div class="poll" :id="poll.id">
@@ -171,6 +216,13 @@ export default {
       });
       this.polls = this.$store.state.polls;
     },
+    share(id) {
+      navigator.clipboard.writeText(id);
+      alert(
+        "Anket numarasını kopyaladınız. Arama çubuğundan anketi bulabilirsiniz " +
+          id
+      );
+    },
     // participantUserControl(id, poll) {
     //   const userid = this.$store.state.loggedInUserId;
 
@@ -245,7 +297,7 @@ $grey_text: #626c72;
 
           .posted-meta {
             display: flex;
-            justify-content: flex-start;
+            justify-content: space-between;
 
             p {
               margin-left: 20px;
@@ -497,5 +549,11 @@ a {
 }
 .dropdown-toggle::after {
   display: none;
+}
+.dropdown-menu {
+  min-width: 0px;
+}
+.dropdown-item {
+  font-weight: inherit !important;
 }
 </style>
