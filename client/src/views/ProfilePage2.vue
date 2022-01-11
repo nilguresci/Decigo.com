53721f62c157bf4a392a23ddc7a42503fc4c92ef
nilguresci <nilguresci@gmail.com>
2022-01-11 19:24:04 +0300

    iyileştirmelr

--- client/src/views/ProfilePage2.vue
+++ client/src/views/ProfilePage2.vue
@@ -9,6 +9,8 @@
 import coverImageComp from "../components/Profile/coverImageComp.vue";
 import pageContentComp from "../components/Profile/pageContentComp.vue";
 import Swal from "sweetalert2";
+import store from "store";
+import router from "../router";
 export default {
   name: "ProfilePage",
   components: {
@@ -18,12 +20,22 @@ export default {
   data() {
     return {
       errorMsg: "",
+      token: "",
     };
   },
   mounted() {
     this.$store.dispatch({
       type: "setMyPolls",
     });
+
+    this.token = store.get("token") ? store.get("token") : "";
+
+    if (this.token === "") {
+      this.$store.state.errorMsg = "Lütfen tekrar giriş yapın.";
+      setTimeout(() => {
+        router.push({ name: "Home" });
+      }, 2000);
+    }
 
     this.$store.watch(
       () => this.$store.state.errorMsg,
